bbb51dedaf438e5b2fc6d5608743ab76fd6c708e
Joe McBride <jmcbride@rackspace.com>
2014-05-09 09:54:54 -0500

    initial commit

---
+++ copy_designate_to_USB.sh
@@ -0,0 +1,27 @@
+#!/bin/bash 
+# This script will probably onle work for a mac
+
+# must be 8 characters or less
+USB_NAME="DESIG"
+OLD_NAME=DISK_IMG
+
+date_append="$(date +%s)"
+date_append=${date_append#1399}
+
+USB_NAME=$USB_NAME$date_append
+path_to_usb=/Volumes/$USB_NAME/
+
+echo "Renaming USB drive to $USB_NAME"
+echo $USB_NAME
+diskutil rename $OLD_NAME $USB_NAME
+
+echo "copying files over"
+#cp -R USB/*.* /Volumes/$USB_NAME/
+rsync -r --progress USB/ $path_to_usb
+
+echo "Ejecting drive."
+diskutil eject $USB_NAME
+#umount -f /Volumes/$USB_NAME
+
+echo "Done!"
+say "Done"
